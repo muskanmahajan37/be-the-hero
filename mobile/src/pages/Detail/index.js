7a90145f0590b3621b4b50f1eceb1103e678610b
Robson Oliveira <Robson_Oliverjr@outlook.com>
2020-03-30 01:45:14 -0300

    Atualizações e reparos

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -41,11 +41,15 @@ export default function Detail() {
                 </TouchableOpacity>
             </View>
             <View style={styles.incident}>
-            <Text style={[styles.incidentProperty, { marginTop:0 }]}>Ong</Text>
+            <Text style={[styles.incidentProperty, { marginTop:0 }]}>ONG</Text>
             <Text style={styles.incidentValue}>{incident.name} de {incident.city}/{incident.uf}</Text>
 
                 <Text style={styles.incidentProperty}>CASO:</Text>
                 <Text style={styles.incidentValue}>{incident.title}</Text>
+
+                <Text style={styles.incidentProperty}>DESCRIÇÃO:</Text>
+                <Text style={styles.incidentValue}>{incident.description}</Text>
+
 
                 <Text style={styles.incidentProperty}>VALOR:</Text>
                     <Text style={styles.incidentValue}>
